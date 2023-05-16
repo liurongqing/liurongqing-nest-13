ce14af8477fcaf5f4ccc968ee1b8bc7118266985
liurongqing <liurongqing@liurongqingdeiMac.local>
2023-05-17 00:09:04 +0800

    1

---
+++ app/sandpack/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Sandpack } from "@codesandbox/sandpack-react";
+
+export default function App() {
+  const files = {};
+  return <Sandpack files={files} theme="auto" template="nextjs" />;
+}
