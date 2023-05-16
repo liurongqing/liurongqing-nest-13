ec053caae089717abf4fd5bea4282517d59af5fe
liurongqing <liurongqing@liurongqingdeiMac.local>
2023-05-17 00:47:43 +0800

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { MDXProvider } from "@mdx-js/react";
 import HelloWorld from "./hello.mdx";
-
 
 export default function Page() {
   return (
-    // <MDXProvider>
       <HelloWorld />
-    // </MDXProvider>
   );
 }
--- app/sandpack/page.tsx
+++ app/sandpack/page.tsx
@@ -3,6 +3,19 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
 
 export default function App() {
-  const files = {};
-  return <Sandpack files={files} theme="auto" template="nextjs" />;
+  const files = {
+    "/page.tsx": `"use client";
+
+    import HelloWorld from "./hello.mdx";
+    
+    export default function Page() {
+      return (
+          <HelloWorld />
+      );
+    }
+    `,
+    "hello.mdx": `## 123`,
+    "app/index.js": "console.log(123)"
+  };
+  return <Sandpack files={files} theme="auto" template="nextjs"/>;
 }
