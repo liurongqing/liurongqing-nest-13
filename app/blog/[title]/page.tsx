a54a3c05e002022635246537c8168d4880e160b5
rongqingliu <phpgege@gmail.com>
2023-05-20 14:08:47 +0800

    gohome

--- app/blog/[title]/page.tsx
+++ app/blog/[title]/page.tsx
@@ -1,10 +1,20 @@
 import { MDXWrapper } from "@/components/MDXWrapper";
-import PageMDX from "./page.mdx";
+// import PageMDX from "./page.mdx";
 export default function Page({ params }) {
-  console.log( 'params001', params );
+  console.log("params001", params);
   return (
-    <MDXWrapper>
-      <PageMDX />
-    </MDXWrapper>
+    // <MDXWrapper>
+    //   <PageMDX />
+    // </MDXWrapper>
+    <h1>123</h1>
   );
 }
+
+// export async function generateStaticParams() {
+//   // const posts = await fetch('https://.../posts').then((res) => res.json());
+//   const posts = [{ slug: "test" }, { slug: "test2" }];
+
+//   return posts.map((post) => ({
+//     slug: post.slug,
+//   }));
+// }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,14 +3,19 @@
 import { Pagination } from "@/components/Pagination";
 import PageMDX from "./page.mdx";
 import { MDXWrapper } from "@/components/MDXWrapper";
+// import { getAllPosts } from "@/lib/getPostsFilePaths";
+import { useEffect } from "react";
 
 export default function Page() {
   console.log(123);
+  useEffect(() => {
+    // getAllPosts([]);
+  }, []);
   return (
     <>
       <div>
         <div className="flex">
-          <div >2022-10-22</div>
+          <div>2022-10-22</div>
           <div>
             <div>title</div>
             <div>descript</div>
@@ -21,3 +26,17 @@ export default function Page() {
     </>
   );
 }
+
+// export const getStaticProps = async () => {
+//   const posts = getAllPosts([
+//     "title",
+//     "slug",
+//     "date",
+//     "description",
+//     "thumbnail",
+//   ]);
+//   console.log("posts", { posts });
+
+//   // retunr the posts props
+//   return { props: { posts } };
+// };
