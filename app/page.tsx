56cb06e8f672e64d566f497def3a6f664eb9b31e
rongqingliu <phpgege@gmail.com>
2023-05-20 16:23:34 +0800

    feat: 快速提交

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,42 @@
-import { MDXRemote } from "next-mdx-remote/rsc";
+// app/page.tsx
+import Link from "next/link";
+import { compareDesc, format, parseISO } from "date-fns";
+import { allPosts, Post } from "contentlayer/generated";
 
-// app/page.js
-export default function Home() {
+function PostCard(post: Post) {
   return (
-    // @ts-expect-error Server Component
-    <MDXRemote
-      source={`# Hello World
+    <div className="mb-8">
+      <h2 className="mb-1 text-xl">
+        <Link
+          href={post.url}
+          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
+        >
+          {post.title}
+        </Link>
+      </h2>
+      <h3>{post.description}</h3>
+      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+        {format(parseISO(post.date), "LLLL d, yyyy")}
+      </time>
+    </div>
+  );
+}
 
-      This is from Server Components!
-      `}
-    />
+export default function Home() {
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  );
+
+  console.log("posts", { posts });
+
+  return (
+    <div className="mx-auto max-w-xl py-8">
+      <h1 className="mb-8 text-center text-2xl font-black">
+        Next.js + Contentlayer Example
+      </h1>
+      {posts.map((post, idx) => (
+        <PostCard key={idx} {...post} />
+      ))}
+    </div>
   );
 }
